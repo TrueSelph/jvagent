0b35bd1e3be45f86f1d61562f24e399e4329f2d7
Isoke <isoke.britton@v75inc.com>
2025-12-19 17:05:33 -0400

    added interview action and minor updates

---
+++ jvagent/action/interview/__init__.py
@@ -0,0 +1,13 @@
+"""Interview Interact Action Package
+
+This package provides the InterviewInteractAction class and its API endpoints.
+"""
+
+# Import the action class so it can be imported from the package
+from .interview_interact_action import InterviewInteractAction
+
+# Import endpoints module to ensure endpoints are discovered and registered
+# This must be imported for endpoint discovery to work
+# from . import endpoints  # noqa: F401
+
+__all__ = ["InterviewInteractAction"]
---
+++ jvagent/action/interview/gather_info_interact_action/__init__.py
@@ -0,0 +1,6 @@
+"""GatherInfoInteractAction interact action package."""
+
+from .gather_info_interact_action import GatherInfoInteractAction
+from .data_node import DataNode
+
+__all__ = ["GatherInfoInteractAction", "DataNode"]
