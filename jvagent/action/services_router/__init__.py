492b5d24204f9aeff0e3cd824c56fff294a4210b
Isoke <isoke.britton@v75inc.com>
2026-02-18 23:32:47 -0400

    removing services action

--- jvagent/action/services_router/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Router action package for finding and connecting to services."""
-
-from .service_directory_interact_action import ServiceDirectoryInteractAction
-
-__all__ = ["ServiceDirectoryInteractAction"]
