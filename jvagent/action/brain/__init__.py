eec593308897e3346ccb2ec38823ce60e6837c39
Isoke <isoke.britton@v75inc.com>
2026-04-17 12:49:17 -0400

    removing old actions

--- jvagent/action/brain/__init__.py
+++
@@ -1,5 +0,0 @@
-"""BrainAction for proactive memory and task scheduling."""
-
-from jvagent.action.brain.brain_action import BrainAction
-
-__all__ = ["BrainAction"]
--- jvagent/action/human_revision/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Human-in-the-Loop Revision Action package."""
-
-from .human_revision_action import HumanRevisionAction, RevisionRequest, RevisionOutcome
-
-__all__ = ["HumanRevisionAction", "RevisionRequest", "RevisionOutcome"]
